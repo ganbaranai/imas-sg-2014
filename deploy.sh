f39b638ea18a7aedeb18a44f3dc3f2ea3ae26cec
David Siaw <davidsiaw@gmail.com>
2014-11-16 11:19:22 +0900

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cp known_hosts ~/.ssh/known_hosts
+chmod 700 deploy-key
 rsync -avz --delete --exclude deploy-key --exclude .git --exclude .gitignore -e 'ssh -i deploy-key' . davidsiaw@astrobunny.net:~/html/imas/
